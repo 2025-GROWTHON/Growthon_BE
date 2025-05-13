e88aed13b33555a779bd20d1b51449aaee845ff4
bum0w0 <kgy5988@naver.com>
2025-05-13 17:31:38 +0900

    [Init] 프로젝트 초기 세팅

---
+++ src/main/java/com/growthon/Farm2YouApplication.java
@@ -0,0 +1,13 @@
+package com.growthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farm2YouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Farm2YouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/growthon/Farm2You/Farm2YouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growthon.Farm2You;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farm2YouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
