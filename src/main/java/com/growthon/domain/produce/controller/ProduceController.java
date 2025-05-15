496f8458085257a36f287190d8afbfaa54b452d7
Junyong <lhimjy1105@naver.com>
2025-05-15 11:23:57 +0900

    #2 [Chore] Swagger testCode 추가

---
+++ src/main/java/com/growthon/domain/produce/controller/ProduceController.java
@@ -0,0 +1,14 @@
+package com.growthon.domain.produce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProduceController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "test";
+    }
+
+}
